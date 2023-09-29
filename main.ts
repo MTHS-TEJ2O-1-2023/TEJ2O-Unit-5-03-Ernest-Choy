ac57fb5744e13b261f05b093f95d7159b363bf53
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-09-29 12:45:12 -0400

    Type script ver

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ernest
+ * Created on: Sep 2023
+ * This program Turns LED on and off
 */
+// On start
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+pins.digitalWritePin(DigitalPin.P16,0)
 
-basic.showString('Hello, World!')
+// Turn on LED
+input.onButtonPressed(Button.A, function() {
+  basic.showIcon(IconNames.Yes)
+  pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+// Turn off LED 
+input.onButtonPressed(Button.B, function () {
+  basic.showIcon(IconNames.No)
+  pins.digitalWritePin(DigitalPin.P16, 0)
+})
